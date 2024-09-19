37224b8de60edf0e6599513e1d9f58b008e1119e
shivamdem003 <1998.saha.shivam@gmail.com>
2024-09-19 19:36:36 +0530

    change in main file

--- src/main/java/com/cronjobs/execution/cronjobs/CronjobsApplication.java
+++ src/main/java/com/cronjobs/execution/cronjobs/CronjobsApplication.java
@@ -2,12 +2,20 @@ package com.cronjobs.execution.cronjobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CronjobsApplication {
+public class CronjobsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(CronjobsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CronjobsApplication.class, args);
